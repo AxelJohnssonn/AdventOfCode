541bf6ad011e636e00d03d2eb6e5ed8fd21bff67
Axel Johnsson <mr.axjo3@gmail.com>
2023-12-10 18:09:28 +0100

    prep day9,

---
+++ adventofcode2023/src/main/java/com/adventofcode/Day9/Day9.java
@@ -0,0 +1,26 @@
+package com.adventofcode.Day9;
+
+import java.util.ArrayList;
+
+import com.Aoc;
+
+public class Day9 extends Aoc{
+    public Day9(String fileName) {
+        super(fileName);
+    }
+
+    public static void main(String[] args) {
+        new Day9("src//main//java//com//adventofcode//Day9//input.txt");
+    }
+
+    @Override
+    protected String part1(ArrayList<String> input) {
+        return null;
+    }
+
+    @Override
+    protected String part2(ArrayList<String> input) {
+        return null;
+    }
+    
+}
