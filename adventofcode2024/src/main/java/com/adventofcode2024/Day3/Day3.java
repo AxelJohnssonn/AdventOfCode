95cfb644e5cd70096ab89eda354b2e0f1511aacb
Axel Johnsson <mr.axjo3@gmail.com>
2024-12-04 21:25:24 +0100

    Oneliner for day3

--- adventofcode2024/src/main/java/com/adventofcode2024/Day3/Day3.java
+++ adventofcode2024/src/main/java/com/adventofcode2024/Day3/Day3.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import com.Aoc;
 import com.adventofcode2024.Day2.Day2;
@@ -21,20 +22,14 @@ public class Day3 extends Aoc {
 
     @Override
     protected String part1(ArrayList<String> input) {
-        StringBuilder sb = new StringBuilder();
-        input.forEach(s -> sb.append(s));
-        String pattern = "mul\\(\\d+,\\d+\\)";
-        Pattern cPattern = Pattern.compile(pattern);
-        Matcher matcher = cPattern.matcher(sb.toString());
-
-        int sum = 0;
-        while (matcher.find()) {
-            String match = matcher.group();
-            int first = Integer.parseInt(match.substring(4, match.length() - 1).split(",")[0]);
-            int second = Integer.parseInt(match.substring(4, match.length() - 1).split(",")[1]);
-            sum += first * second;
-        }
-        return String.valueOf(sum);
+        return String.valueOf(Pattern.compile("mul\\(\\d+,\\d+\\)")
+                .matcher(input.stream().map(Object::toString).collect(Collectors.joining("")))
+                .results()
+                .map(s -> s.group()).collect(Collectors.toList())
+                .stream()
+                .mapToInt(s -> Integer.parseInt(s.substring(4, s.length() - 1).split(",")[0])
+                        * Integer.parseInt(s.substring(4, s.length() - 1).split(",")[1]))
+                .sum());
     }
 
     @Override
