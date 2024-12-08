0b6fc012db6b36a5e1428478c9b160514185e1fa
Axel Johnsson <mr.axjo3@gmail.com>
2024-12-08 12:32:54 +0100

    Day 7 Done

--- adventofcode2024/src/main/java/com/adventofcode2024/Day5/Day5.java
+++ adventofcode2024/src/main/java/com/adventofcode2024/Day5/Day5.java
@@ -1,21 +1,109 @@
 package com.adventofcode2024.Day5;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import com.Aoc;
+import com.adventofcode2024.Day4.Day4;
 
 public class Day5 extends Aoc {
+
+    private List<List<Integer>> invalidList;
+
     protected Day5(String fileName) {
         super(fileName);
     }
 
+    public static void main(String[] args) {
+        new Day5("5");
+    }
+
     @Override
     protected String part1(ArrayList<String> input) {
-        return "";
+
+        Map<Integer, List<Integer>> rules = new HashMap<>();
+        invalidList = new ArrayList<>();
+        for (String s : input.subList(0, input.indexOf(""))) {
+            int key = Integer.parseInt(s.split("\\|")[0]);
+            int value = Integer.parseInt(s.split("\\|")[1]);
+            rules.compute(key, (k, v) -> {
+                if (v == null) {
+                    v = new ArrayList<>();
+                }
+                v.add(value);
+                return v;
+            });
+        }
+        int sum = 0;
+        for (String s : input.subList(input.indexOf("") + 1, input.size())) {
+            boolean valid = true;
+            List<Integer> printedPages = new ArrayList<>();
+            for (String page : Arrays.stream(s.split(",")).toList()) {
+
+                List<Integer> rule = rules.get(Integer.parseInt(page));
+                if (rule != null && printedPages.stream().anyMatch(rule::contains)) {
+                    valid = false;
+                }
+
+                printedPages.add(Integer.parseInt(page));
+
+            }
+
+            if (valid) {
+                sum += Integer.parseInt(
+                        Arrays.stream(s.split(",")).toList().get(Arrays.stream(s.split(",")).toList().size() / 2));
+            } else {
+                invalidList.add(
+                        Arrays.stream(s.split(",")).mapToInt(p -> Integer.parseInt(p)).boxed()
+                                .collect(Collectors.toList()));
+            }
+        }
+
+        return String.valueOf(sum);
     }
 
     @Override
     protected String part2(ArrayList<String> input) {
-        return "";
+        Map<Integer, List<Integer>> rules = new HashMap<>();
+
+        for (String s : input.subList(0, input.indexOf(""))) {
+            int key = Integer.parseInt(s.split("\\|")[0]);
+            int value = Integer.parseInt(s.split("\\|")[1]);
+            rules.compute(key, (k, v) -> {
+                if (v == null) {
+                    v = new ArrayList<>();
+                }
+                v.add(value);
+                return v;
+            });
+        }
+
+        for (List<Integer> invalid : invalidList) {
+            for (int i = invalid.size() - 1; i >= 0; i--) {
+                int page = invalid.get(i);
+
+                List<Integer> rule = rules.get(page);
+                if (rule != null) {
+                    for (int target : rule) {
+                        int targetIndex = invalid.indexOf(target);
+                        if (targetIndex != -1 && targetIndex < i) {
+                            int tmp = invalid.get(targetIndex);
+                            invalid.set(targetIndex, page);
+                            invalid.set(i, tmp);
+                            i++;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return String.valueOf(invalidList.stream().mapToInt(invalid -> invalid.get((invalid.size() - 1) / 2)).sum());
+
     }
 }
--- adventofcode2024/src/main/java/com/adventofcode2024/Day7/Day7.java
+++ adventofcode2024/src/main/java/com/adventofcode2024/Day7/Day7.java
@@ -1,21 +1,73 @@
 package com.adventofcode2024.Day7;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import com.google.common.primitives.Longs;
 
 import com.Aoc;
+import com.adventofcode2024.Day5.Day5;
 
 public class Day7 extends Aoc {
     protected Day7(String fileName) {
         super(fileName);
     }
 
+    public static void main(String[] args) {
+        new Day7("7");
+    }
+
     @Override
     protected String part1(ArrayList<String> input) {
-        return "";
+
+        List<List<Long>> rows = input.stream()
+                .map(line -> Arrays.stream(line.split(":? ")).map(Long::parseLong).toList()).toList();
+
+        Long sum = (long) 0;
+        for (List<Long> row : rows) {
+            if (isValid(row, row.get(0), row.get(1), 2)) {
+                sum += row.get(0);
+            }
+
+        }
+
+        return String.valueOf(sum);
+    }
+
+    public boolean isValid(List<Long> row, Long result, Long current, int index) {
+        if (index == row.size()) {
+            return current.longValue() == result.longValue();
+        }
+        return isValid(row, result, current + row.get(index), index + 1)
+                || isValid(row, result, current * row.get(index), index + 1);
+
+    }
+
+    public boolean isValidPart2(List<Long> row, Long result, Long current, int index) {
+        if (index == row.size()) {
+            return current.longValue() == result.longValue();
+        }
+        return isValidPart2(row, result, current + row.get(index), index + 1)
+                || isValidPart2(row, result, current * row.get(index), index + 1)
+                || isValidPart2(row, result, Long.parseLong(String.format("%d%d", current, row.get(index))), index + 1);
+
     }
 
     @Override
     protected String part2(ArrayList<String> input) {
-        return "";
+        List<List<Long>> rows = input.stream()
+                .map(line -> Arrays.stream(line.split(":? ")).map(Long::parseLong).toList()).toList();
+
+        Long sum = (long) 0;
+        for (List<Long> row : rows) {
+            if (isValidPart2(row, row.get(0), row.get(1), 2)) {
+                sum += row.get(0);
+            }
+
+        }
+
+        return String.valueOf(sum);
     }
 }
